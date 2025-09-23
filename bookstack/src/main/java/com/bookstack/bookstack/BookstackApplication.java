74c4af619f235f8c53f77798141de19d0893b396
Patrick Wu <patrickwu8894@gmail.com>
2025-09-23 06:19:48 -0700

    Set up java spring boot environment

---
+++ bookstack/src/main/java/com/bookstack/bookstack/BookstackApplication.java
@@ -0,0 +1,13 @@
+package com.bookstack.bookstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstackApplication.class, args);
+	}
+
+}
